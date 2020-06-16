f81db91f5384a972f6a2c2386cc7d10ce4d5977b
asim samuel <asim.samuel1@gmail.com>
2020-06-16 11:17:33 -0400

    added validation on Order Item

--- server/db/models/orderItem.js
+++ server/db/models/orderItem.js
@@ -4,6 +4,10 @@ const db = require('../db');
 const OrderItem = db.define('orderItem', {
   quantity: {
     type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+      max: Infinity
+    }
   }
 
 });
