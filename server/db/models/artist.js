ce824af8e06a67384cf77b75144ad091fd5588fb
Claudia Saimbert <csaimbert@gmail.com>
2020-06-16 11:17:25 -0400

    created seed-artist & seed-product files

--- server/db/models/artist.js
+++ server/db/models/artist.js
@@ -10,26 +10,3 @@ module.export = db.define("artist", {
         },
     },
 });
-
-// add to seed.js
-const artists = [
-    {
-        name: "ANJEL - Boris Anje Tabufor",
-    },
-    { name: "Benny Bing" },
-    {
-        name: "Claudia Saimbert",
-    },
-    {
-        name: "David Thuku",
-    },
-    {
-        name: "Deana Lawson",
-    },
-    {
-        name: "Micaiah Carter",
-    },
-    {
-        name: "Mickalene Thomas",
-    },
-];
