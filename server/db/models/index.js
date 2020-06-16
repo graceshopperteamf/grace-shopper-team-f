23170442e67d32a4d25b678f3b2dd41365aedeaa
asim samuel <asim.samuel1@gmail.com>
2020-06-16 15:16:23 -0400

    updates for merge

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,15 +1,13 @@
 const db = require('../db');
 const User = require('./user');
-const Order = require('./order');
-const OrderItem = require('./orderItem');
+
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
  */
-OrderItem.belongsTo(Order);
-Order.hasMany(OrderItem);
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -19,6 +17,5 @@ Order.hasMany(OrderItem);
 module.exports = {
   db,
   User,
-  Order,
-  OrderItem
+
 };
