cfb53781ef52eedbd1ecfbbb8b2fc2577b654de4
asim samuel <asim.samuel1@gmail.com>
2020-06-16 17:06:03 -0400

    new models created, testing for orders and order items, updated dummy data

--- server/db/models/order.spec.js
+++ server/db/models/order.spec.js
@@ -1,12 +1,52 @@
-// const { expect } = require('chai');
-// const db = require('../index');
-// const Order = db.model('order');
-// const OrderItem = db.model('orderItem');
+const chai = require('chai');
+const expect = chai.expect;
+const db = require('../db');
+const Order = require('./order.js');
+const OrderItem = require('./orderItem');
 
-// describe('Order model', () => {
-//   let order = Order;
-//   it('checks for totalPrice key', () => {
-//     expect(order(order.totalPrice)).to.be.at.least(1);
-//   });
 
-// });
+describe('Order Validation', () => {
+  it('contains status', async () => {
+    const newOrder = await Order.create({
+      status: false,
+      isCart: false
+
+
+    });
+    expect(newOrder.status).to.equal(false);
+    expect(newOrder.isCart).to.equal(false);
+
+
+  });
+
+  it('requires boolean,', async () => {
+    const newOrder = await Order.create({
+      status: true
+    });
+    expect(newOrder.status).to.be.a('boolean');
+  });
+
+
+});
+//Order Item
+describe('Order Item Validation', () => {
+  it('contains quantity', async () => {
+    const newOrder = await OrderItem.create({
+      quantity: 2
+
+
+    });
+    expect(newOrder.quantity).to.equal(2);
+
+  });
+
+  it('requires an number,', async () => {
+    const newOrder = await OrderItem.create({
+      quantity: 1
+    });
+    expect(newOrder.quantity).to.be.a('number');
+  });
+
+
+});
+
