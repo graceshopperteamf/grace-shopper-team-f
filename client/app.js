d6f925b8cb2a1b7dd3ba0b23514d9db8ee1be97c
Kristy Li <kristyli20.0@gmail.com>
2020-06-17 14:29:51 -0400

    Render AllProducts component to display all products

--- client/app.js
+++ client/app.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import {Navbar} from './components';
 import Routes from './routes';
+import AllProducts from './components/AllProducts';
+import products from '../script/seed-product';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <AllProducts products={products} />
     </div>
   );
 };
