41c59a6d0d2a0c0f14b0c31575975a2fd8ae9d79
Kristy Li <kristyli20.0@gmail.com>
2020-06-17 15:19:58 -0400

    Remove import line

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 const Sequelize = require('sequelize');
 const db = require('../db');
-const Order = require('./order');
 
 const User = db.define('user', {
     name: {
