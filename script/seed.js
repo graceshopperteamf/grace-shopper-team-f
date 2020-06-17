9a02fd7a1633e3170afa7be6b53765139937dd17
asim samuel <asim.samuel1@gmail.com>
2020-06-17 17:36:28 -0400

    adjusted seeding

--- script/seed.js
+++ script/seed.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 'use strict';
 
 const db = require('../server/db');
@@ -17,22 +18,50 @@ const seed = async () => {
         await db.sync({ force: true });
         console.log('db synced!');
 
-        await Promise.all(arrayOfUsers.map(user => {
+        let users = await Promise.all(arrayOfUsers.map(user => {
             return User.create(user);
         }));
-        await Promise.all(ArtistSeed.map(artist => {
+        let art = await Promise.all(ArtistSeed.map(artist => {
+
             return Artist.create(artist);
         }));
-        await Promise.all(ProductSeed.map(product => {
+        let prods = await Promise.all(ProductSeed.map(product => {
             return Product.create(product);
         }));
 
-        await Promise.all(OrderSeed.map(order => {
+        let cart = await Promise.all(OrderSeed.map(order => {
             return Order.create(order);
         }));
-        await Promise.all(OrderItemSeed.map(item => {
+        let items = await Promise.all(OrderItemSeed.map(item => {
             return OrderItem.create(item);
         }));
+        // eslint-disable-next-line no-proto
+
+        //artitst has products
+        // console.log(prods[0].__proto__);
+
+        for (let i = 0; i < art.length; i++) {
+            let artist = art[i];
+            for (let j = 0; j < 4; j++) {
+                let test = prods[i * 4 + j];
+                await artist.addProduct(test);
+
+            }
+
+        }
+
+        // for (let i = 0; i < cart.length; i++) {
+        //     let order = cart[i];
+
+        // };
+
+        // await cart.addOrderItem(item[0]);
+
+        console.log(art, 'ARTTTTT');
+        // console.log(cart[0].__proto__);
+        // console.log(cart[0].__proto__);
+
+        // eslint-disable-next-line no-proto
 
         console.log(`seeded successfully`);
     } catch (err) {
