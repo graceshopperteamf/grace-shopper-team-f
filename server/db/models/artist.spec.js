9b7f5573fd79e0a1fef0d85a47a0f2ea1891ce0b
Claudia Saimbert <csaimbert@gmail.com>
2020-06-16 21:51:20 -0400

    completed Artist Test

--- server/db/models/artist.spec.js
+++ server/db/models/artist.spec.js
@@ -1,3 +1,28 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const db = require('../index');
 const Artist = db.model('artist');
+
+describe('Artist Model', () => {
+    let artist;
+    beforeEach(() => {
+        artist = {
+            name: 'Claudia Saimbert',
+        };
+        return db.sync({ force: true });
+    });
+    it('has field name', async () => {
+        const newArtist = await Artist.create(artist);
+        expect(newArtist.name).to.equal('Claudia Saimbert');
+    });
+    it('requires `name`', async () => {
+        const blankArtist = Artist.build();
+        try {
+            await blankArtist.validate();
+            throw Error(
+                'WOMP WOMP, validation should have failed without name'
+            );
+        } catch (err) {
+            expect(err.message).to.contain('name cannot be null');
+        }
+    });
+});
