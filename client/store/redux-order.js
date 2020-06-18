9ae1feed8ad9b355063ba6b9e26e41cfe7a697b9
Claudia Saimbert <csaimbert@gmail.com>
2020-06-18 12:24:35 -0400

    deleted un neccessary tests

--- client/store/redux-order.js
+++ client/store/redux-order.js
@@ -1,43 +1,44 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// //[] Get all orders
-// // [] Make all orders accessible to Admin
-// // [] Get all orders for a certain User
 
-// /**
-//  * ACTION TYPES
-//  */
-// export const GET_ORDERS = 'GET_ORDERS';
+// [] Get all orders for a certain User
+// For Admin only
+/**
+ * ACTION TYPES
+ */
+export const GET_ORDERS = 'GET_ORDERS';
 
-// /**
-//  * ACTION CREATORS
-//  */
-// export const getOrders = (orders) => ({ type: 'GET_ORDERS', orders });
+/**
+ * ACTION CREATORS
+ */
+export const getOrders = (orders) => ({ type: 'GET_ORDERS', orders });
 
-// /**
-//  * THUNK CREATORS
-//  */
-// export const fetchOrders = () => async (dispatch) => {
-//   try {
-//     const { data } = await axios.get('/server/api/orders');
-//     dispatch(getOrders(data));
-//   } catch (error) {
-//     console.log('Cool cliques throw bricks but seldom hit targets.');
-//   }
-// };
-// /**
-//  * INITIAL STATE
-//  */
-// const initialState = [];
-// /**
-//  * REDUCER
-//  */
+//
 
-// export default function orderReducer(state = initialState, action) {
-//   switch (action.type) {
-//     case GET_ORDERS:
-//       return action.orders;
-//     default:
-//       return state;
-//   }
-// }
+/**
+ * THUNK CREATORS
+ */
+export const fetchOrders = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get('/server/api/orders');
+    dispatch(getOrders(data));
+  } catch (error) {
+    console.log(error, 'Cool cliques throw bricks but seldom hit targets.');
+  }
+};
+/**
+ * INITIAL STATE
+ */
+const initialState = [];
+/**
+ * REDUCER
+ */
+
+export default function orderReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_ORDERS:
+      return action.orders;
+    default:
+      return state;
+  }
+}
