ae4724a6b273d0258eeec3387bacb5a2e10100a1
Kristy Li <kristyli20.0@gmail.com>
2020-06-18 12:22:33 -0400

    Add additional routes

--- client/routes.js
+++ client/routes.js
@@ -1,38 +1,39 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch } from "react-router-dom";
-import PropTypes from "prop-types";
-import { Login, Signup, UserHome, AllProducts } from "./components";
-import { me } from "./store";
-import products from "../script/seed-product"; // temp
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Login, Signup, UserHome, AllProducts } from './components';
+import { me } from './store';
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
     componentDidMount() {
+        console.log("routes component is mounting")
         this.props.loadInitialData();
     }
 
     render() {
         const { isLoggedIn } = this.props;
+        console.log(this.props.location);
 
         return (
             <Switch>
                 {/* Routes placed here are available to all visitors */}
+                <Route exact path="/products" component={AllProducts} />
+                <Route exact path="/" component={Login} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={Signup} />
-                <Route path="/products">
-                    <AllProducts products={products} />
-                </Route>
                 {isLoggedIn && (
                     <Switch>
                         {/* Routes placed here are only available after logging in */}
-                        <Route path="/home" component={UserHome} />
+                        <Route exact path="/home" component={UserHome} />
+
                     </Switch>
                 )}
                 {/* Displays our Login component as a fallback */}
-                <Route exact component={Login} />
+                <Route exact component={AllProducts} />
             </Switch>
         );
     }
@@ -53,7 +54,7 @@ const mapDispatch = (dispatch) => {
     return {
         loadInitialData() {
             dispatch(me());
-        },
+        }
     };
 };
 
