a32b6a6b07b36e749941dd0856ba565f7e9d880f
Kristy Li <kristyli20.0@gmail.com>
2020-06-18 12:21:41 -0400

    Reformat file

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Navbar} from './components';
 import Routes from './routes';
+import { Navbar } from './components';
 import Container from '@material-ui/core/Container';
 
 const App = () => {
