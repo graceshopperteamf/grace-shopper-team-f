3021b34642a7231cb8315f8a6768c6ff143b869c
Kristy Li <kristyli20.0@gmail.com>
2020-06-18 15:41:15 -0400

    Fix routes and remove comments

--- client/routes.js
+++ client/routes.js
@@ -5,47 +5,32 @@ import PropTypes from 'prop-types';
 import { Login, Signup, UserHome, AllProducts } from './components';
 import { me } from './store';
 
-/**
- * COMPONENT
- */
 class Routes extends Component {
     componentDidMount() {
-        console.log("routes component is mounting")
         this.props.loadInitialData();
     }
 
     render() {
         const { isLoggedIn } = this.props;
-        console.log(this.props.location);
 
         return (
             <Switch>
-                {/* Routes placed here are available to all visitors */}
-                <Route exact path="/products" component={AllProducts} />
-                <Route exact path="/" component={Login} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/products" component={AllProducts} />
                 {isLoggedIn && (
                     <Switch>
-                        {/* Routes placed here are only available after logging in */}
-                        <Route exact path="/home" component={UserHome} />
-
+                        <Route path="/home" component={UserHome} />
                     </Switch>
                 )}
-                {/* Displays our Login component as a fallback */}
-                <Route exact component={AllProducts} />
+                <Route component={Login} />
             </Switch>
         );
     }
 }
 
-/**
- * CONTAINER
- */
 const mapState = (state) => {
     return {
-        // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-        // Otherwise, state.user will be an empty object, and state.user.id will be falsey
         isLoggedIn: !!state.user.id,
     };
 };
@@ -54,17 +39,12 @@ const mapDispatch = (dispatch) => {
     return {
         loadInitialData() {
             dispatch(me());
-        }
+        },
     };
 };
 
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
 export default withRouter(connect(mapState, mapDispatch)(Routes));
 
-/**
- * PROP TYPES
- */
 Routes.propTypes = {
     loadInitialData: PropTypes.func.isRequired,
     isLoggedIn: PropTypes.bool.isRequired,
