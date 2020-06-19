428bc53597960faefdc4fcddfe39736175a5a5f0
Andres <andresgomezjr89@gmail.com>
2020-06-19 18:47:50 -0400

    removed orderitems api router from index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/users', require('./users'));
-router.use('/orderItems', require('./orderItems'));
 router.use('/orders', require('./orders'));
 router.use('/artists', require('./artists'));
 router.use('/products', require('./products'));
