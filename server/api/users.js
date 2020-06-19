7c118c72288396f3c3d81ff247400448255d9aa8
Andres <andresgomezjr89@gmail.com>
2020-06-19 16:40:04 -0400

    refactored users api, and eager loaded user field in eager loaded user orders

--- server/api/users.js
+++ server/api/users.js
@@ -5,36 +5,40 @@ const {User, Order} = require('../db/models');
 module.exports = router;
 
 router.get('/', adminMiddleware, async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    });
-    res.json(users);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const users = await User.findAll({ attributes: ['id', 'email'] });
+        res.json(users);
+    }
+    catch (err) {
+        next(err);
+    }
 });
 
 
 router.get('/:userId', adminMiddleware, async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.userId, { attributes: ['id', 'email'] });
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const user = await User.findByPk(req.params.userId, { attributes: ['id', 'email'] });
+        res.json(user);
+    }
+    catch (err) {
+        next(err);
+    }
 });
 
 // get all the orders for the user
 router.get('/:userId/orders', async (req, res, next) => {
-  try {
-      const user = await User.findByPk(req.params.userId, { include: [Order] });
-      const orders = await user.getOrders();
-      res.status(200).json(orders);
-  }
-  catch (e) { next(e); }
+    try {
+        const user = await User.findByPk(req.params.userId, {
+            include: {
+                model: Order,
+                include: {
+                    model: User
+                }
+            }
+        });
+        const orders = user.orders;
+        res.status(200).json(orders);
+    }
+    catch (e) { next(e); }
 });
 
