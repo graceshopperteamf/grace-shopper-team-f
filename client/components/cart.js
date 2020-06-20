8e77097160537b40dacbfcf2f9a21d4608a8ae6d
Claudia Saimbert <csaimbert@gmail.com>
2020-06-20 00:10:21 -0400

    Feature: started Cart components. Waiting on some crucial parts to complete. Also, Need to discuss how to test

---
+++ client/components/cart.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined';
+import SingleProduct from './SingleProduct';
+import { fetchProductsFromServer } from '../store/product';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+//import actions or Local storage function
+//checkout
+// need to add shopping basket somewhere here and on Homepage
+// As a customer/visitor, I want to be able to:
+// [] edit my cart if I change my mind:
+// - change the quantity of a product in my cart.
+// - remove a product in my cart.
+// - No one else should be able to edit my cart except me.
+// [] "checkout" the products in my cart so I can purchase my desired goods.
+//  need action removeProduct
+//single product
+
+class Cart extends React.Component {
+  componentDidMount() {
+    //Asim making  get single product
+    this.props.getAllProducts();
+  }
+  // add & subtract the quantity of product. Via orderItem??
+  //update cart
+  //remove the item
+  handleRemove = (id) => {
+    this.props.removeProduct(id);
+  };
+
+  render() {
+    //tried to test this but I can't
+    //this.props will have all products
+    let Products = this.props.products.length ? (
+      this.props.products.map((product) => {
+        return (
+          <SingleProduct key={product.id}>
+            <li>
+              <div>
+                <image src={product.image} width={300} alt={product.title} />
+                <p>
+                  {/* artist ? */}
+                  Title: {product.title}
+                  Price: ${product.price.toLocaleString('en-US')}
+                  Type: {product.type}
+                  Quantity:
+                </p>
+                <div>{/* <Link onClicks for add and remove quantity/> */}</div>
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.handleRemove(product.id);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            </li>
+          </SingleProduct>
+        );
+      })
+    ) : (
+      <p>
+        Nothing in your order. Turn back and capture your bounty like Elliot
+        Ness!
+      </p>
+    );
+    return (
+      <div>
+        <ul>{Products}</ul>
+      </div>
+    );
+  }
+}
+
+const mapProducts = (state) => {
+  return {
+    products: state.products,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    getAllProducts: () => dispatch(fetchProductsFromServer()),
+  };
+};
+export default connect(mapProducts, mapDispatch)(Cart);
--- client/routes.js
+++ client/routes.js
@@ -2,50 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Login, Signup, UserHome, AllProducts } from './components';
+import { Login, Signup, UserHome, AllProducts, Cart } from './components';
 import { me } from './store';
 
 class Routes extends Component {
-    componentDidMount() {
-        this.props.loadInitialData();
-    }
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
 
-    render() {
-        const { isLoggedIn } = this.props;
+  render() {
+    const { isLoggedIn } = this.props;
 
-        return (
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/products" component={AllProducts} />
-                {isLoggedIn && (
-                    <Switch>
-                        <Route path="/home" component={UserHome} />
-                    </Switch>
-                )}
-                <Route component={Login} />
-            </Switch>
-        );
-    }
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/products" component={AllProducts} />
+        <Route path="/cart" component={Cart} />
+        {isLoggedIn && (
+          <Switch>
+            <Route path="/home" component={UserHome} />
+          </Switch>
+        )}
+        <Route component={Login} />
+      </Switch>
+    );
+  }
 }
 
 const mapState = (state) => {
-    return {
-        isLoggedIn: !!state.user.id,
-    };
+  return {
+    isLoggedIn: !!state.user.id,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-    return {
-        loadInitialData() {
-            dispatch(me());
-        },
-    };
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
 };
 
 export default withRouter(connect(mapState, mapDispatch)(Routes));
 
 Routes.propTypes = {
-    loadInitialData: PropTypes.func.isRequired,
-    isLoggedIn: PropTypes.bool.isRequired,
+  loadInitialData: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
 };
