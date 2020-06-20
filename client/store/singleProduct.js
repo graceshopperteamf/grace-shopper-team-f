7d307ffbc2ffdcc46242a415bebb2a321b89772c
Kristy Li <kristyli20.0@gmail.com>
2020-06-20 16:08:39 -0400

    Add GET_ONE_PRODUCT action type, getOneProductActionCreator action creator, fetchOneProductFromServer thunk, and reducer

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -2,28 +2,23 @@ import axios from 'axios';
 
 export const GET_ONE_PRODUCT = 'GOT_ONE_PRODUCT';
 
-const initialState = {
-
-};
+const initialState = {};
 
 export const getOneProductActionCreator = (product) => ({
-  type: GET_ONE_PRODUCT,
-  product
+    type: GET_ONE_PRODUCT,
+    product,
 });
 
-export const fetchOneProductFromServer = (productId) => {
-  return async (dispatch) => {
+export const fetchOneProductFromServer = (productId) => async (dispatch) => {
     const { data } = await axios.get(`/api/products/${productId}`);
     dispatch(getOneProductActionCreator(data));
-  };
 };
 
 export default function (state = initialState, action) {
-  switch (action.type) {
-    case GET_ONE_PRODUCT:
-      return action.product;
-    default:
-      return state;
-  }
-
+    switch (action.type) {
+        case GET_ONE_PRODUCT:
+            return action.product;
+        default:
+            return state;
+    }
 }
