05854921bed3bbb0dae3c7dff6149f235d216486
Kristy Li <kristyli20.0@gmail.com>
2020-06-20 16:04:19 -0400

    Fix the route path to render a different component and the import statement

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Login, Signup, UserHome, AllProducts, SingleProduct } from './components';
+import {
+    Login,
+    Signup,
+    UserHome,
+    AllProducts,
+    SingleProductView,
+} from './components';
 import { me } from './store';
 
 class Routes extends Component {
@@ -15,7 +21,7 @@ class Routes extends Component {
 
         return (
             <Switch>
-                <Route path="/products/:id" component={SingleProduct} />
+                <Route path="/products/:id" component={SingleProductView} />
                 <Route path="/login" component={Login} />
                 <Route path="/signup" component={Signup} />
                 <Route path="/products" component={AllProducts} />
