e8e5fa1a9d829a5a08f7d82fb08489e755c70eb9
Kristy Li <kristyli20.0@gmail.com>
2020-06-20 15:59:13 -0400

    Fix the props that are passed down into SingleProduct

--- client/components/index.js
+++ client/components/index.js
@@ -7,4 +7,4 @@ export { default as Navbar } from './navbar';
 export { default as UserHome } from './user-home';
 export { Login, Signup } from './auth-form';
 export { default as AllProducts } from './AllProducts';
-export { default as SingleProduct } from './SingleProduct';
+export { default as SingleProductView } from './SingleProductView';
