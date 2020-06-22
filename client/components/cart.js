48640d1b0f59607ff2c520a78e2fa0b58fb45b19
Kristy Li <kristyli20.0@gmail.com>
2020-06-22 01:20:39 -0400

    Chore: Add handleRemoveClick method, handleClearClick method, handleUpdateClick method, fix what the Cart component renders, and connect to the Redux store

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,87 +1,88 @@
 import React from 'react';
-// import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined';
-import SingleProduct from './SingleProduct';
 import { fetchProductsFromServer } from '../store/product';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-//import actions or Local storage function
-//checkout
-// need to add shopping basket somewhere here and on Homepage
-// As a customer/visitor, I want to be able to:
-// [] edit my cart if I change my mind:
-// - change the quantity of a product in my cart.
-// - remove a product in my cart.
-// - No one else should be able to edit my cart except me.
-// [] "checkout" the products in my cart so I can purchase my desired goods.
-//  need action removeProduct
-//single product
+import {
+    removeFromCart,
+    updateItemFromCart,
+    clearCart,
+} from '../store/localStorage';
+import CartForm from './CartForm';
 
 class Cart extends React.Component {
-  componentDidMount() {
-    //Asim making  get single product
-    this.props.getAllProducts();
-  }
-  // add & subtract the quantity of product. Via orderItem??
-  //update cart
-  //remove the item
-  handleRemove = (id) => {
-    this.props.removeProduct(id);
-  };
+    constructor(props) {
+        super(props);
 
-  render() {
-    //tried to test this but I can't
-    //this.props will have all products
-    let Products = this.props.products.length ? (
-      this.props.products.map((product) => {
-        return (
+        this.handleRemoveClick = this.handleRemoveClick.bind(this);
+        this.handleClearClick = this.handleClearClick.bind(this);
+        this.handleUpdateClick = this.handleUpdateClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.getAllProducts();
+    }
+
+    handleRemoveClick(id) {
+        this.props.removeFromCart(id);
+    }
+
+    handleClearClick() {
+        this.props.clearCart();
+    }
+
+    handleUpdateClick(id, quantity) {
+        this.props.updateItemFromCart(id, Number(quantity));
+    }
+
+    render() {
+        const filteredProducts = [];
+
+        for (let i = 0; i < this.props.cart.length; i++) {
+            const idOfCurrentProduct = this.props.cart[i].id;
+            const product = this.props.products.filter(
+                (currentProduct) => currentProduct.id === idOfCurrentProduct
+            )[0];
+            const productWithQuantity = {
+                ...product,
+                quantity: this.props.cart[i].quantity,
+            };
+
+            filteredProducts.push(productWithQuantity);
+        }
+
+        return filteredProducts.length ? (
             <div>
-                <li>
-                    <div>
-                        <img src={product.image} width={300} alt={product.title} />
-                        <p>
-                        artist ?
-                        Title: {product.title}
-                        Price: ${product.price.toLocaleString('en-US')}
-                        Type: {product.type}
-                        Quantity:
-                        </p>
-                        <button
-                        type="button"
-                        onClick={() => {
-                            this.handleRemove(product.id);
-                        }}
-                        >
-                        Remove
-                        </button>
+                {filteredProducts.map((product) => (
+                    <div key={product.id}>
+                        <CartForm product={product} handleRemoveClick={this.handleRemoveClick} handleUpdateClick={this.handleUpdateClick} />
                     </div>
-                </li>
+                ))}
+                <button type="button" onClick={() => this.handleClearClick()}>
+                    Clear
+                </button>
             </div>
+        ) : (
+            <p>
+                Nothing in your order. Turn back and capture your bounty like
+                Elliot Ness!
+            </p>
         );
-      })
-    ) : (
-      <p>
-        Nothing in your order. Turn back and capture your bounty like Elliot
-        Ness!
-      </p>
-    );
-    return (
-      <div>
-        <ul>{Products}</ul>
-      </div>
-    );
-  }
+    }
 }
 
 const mapProducts = (state) => {
-  return {
-    products: state.products,
-  };
+    return {
+        cart: state.cart,
+        products: state.products,
+    };
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    getAllProducts: () => dispatch(fetchProductsFromServer()),
-  };
+    return {
+        getAllProducts: () => dispatch(fetchProductsFromServer()),
+        removeFromCart: (id) => dispatch(removeFromCart(id)),
+        updateItemFromCart: (id, quantity) => dispatch(updateItemFromCart(id, quantity)),
+        clearCart: () => dispatch(clearCart())
+    };
 };
 
 export default connect(mapProducts, mapDispatch)(Cart);
