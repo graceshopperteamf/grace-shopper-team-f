1ea4af9ff651d85d40c4d6e3e09d0099c0965c12
Kristy Li <kristyli20.0@gmail.com>
2020-06-22 17:34:13 -0400

    Chore: Add link to /shopping_cart and remove comments

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -10,16 +10,15 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
         <nav>
             {isLoggedIn ? (
                 <div>
-                    {/* The navbar will show these links after you log in */}
                     <Link to="/home">Home</Link>
                     <a href="#" onClick={handleClick}>
                         Logout
                     </a>
                     <Link to="/products">Products</Link>
+                    <Link to="/shopping_cart">Shopping Cart</Link>
                 </div>
             ) : (
                 <div>
-                    {/* The navbar will show these links before you log in */}
                     <Link to="/login">Login</Link>
                     <Link to="/signup">Sign Up</Link>
                     <Link to="/products">Products</Link>
@@ -31,9 +30,6 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
     </div>
 );
 
-/**
- * CONTAINER
- */
 const mapState = (state) => {
     return {
         isLoggedIn: !!state.user.id,
@@ -50,9 +46,6 @@ const mapDispatch = (dispatch) => {
 
 export default connect(mapState, mapDispatch)(Navbar);
 
-/**
- * PROP TYPES
- */
 Navbar.propTypes = {
     handleClick: PropTypes.func.isRequired,
     isLoggedIn: PropTypes.bool.isRequired,
