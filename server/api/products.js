f570c690ed50fca34d9fee59e6dd30fa8d2e092a
Kristy Li <kristyli20.0@gmail.com>
2020-06-22 01:14:50 -0400

    Style: Reformat using Prettier and ESLint

--- server/api/products.js
+++ server/api/products.js
@@ -8,35 +8,41 @@ router.get('/', async (req, res, next) => {
     try {
         const products = await Product.findAll({ include: [Artist] });
         res.status(200).json(products);
+    } catch (e) {
+        next(e);
     }
-    catch (e) { next(e); }
 });
-
 
 router.get('/:productId', async (req, res, next) => {
     try {
-        const product = await Product.findByPk(req.params.productId, { include: [Artist] });
+        const product = await Product.findByPk(req.params.productId, {
+            include: [Artist],
+        });
         res.status(200).json(product);
+    } catch (e) {
+        next(e);
     }
-    catch (e) { next(e); }
 });
 
 router.delete('/:productId', adminMiddleware, async (req, res, next) => {
     try {
-        await Product.destroy( { where: { id: req.params.productId } });
+        await Product.destroy({ where: { id: req.params.productId } });
         res.status(204).end();
+    } catch (e) {
+        next(e);
     }
-    catch (e) { next(e); }
 });
 
 router.put('/:productId', adminMiddleware, async (req, res, next) => {
     try {
-        let product = await Product.findByPk(req.params.productId, { include: [Artist] });
+        let product = await Product.findByPk(req.params.productId, {
+            include: [Artist],
+        });
         product = await product.update(req.body);
         res.status(200).json(product);
+    } catch (e) {
+        next(e);
     }
-    catch (e) { next(e); }
 });
-
 
 module.exports = router;
