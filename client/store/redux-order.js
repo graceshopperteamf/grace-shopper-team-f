7d74473aac2266b3c652cfc8a71b393bca8ecd0f
Claudia Saimbert <csaimbert@gmail.com>
2020-06-23 22:47:13 -0400

    Fix: Checkout Component was not rendering. I removed Redux form and intergrated material Ui. Need to add functionality and more fields

--- client/store/redux-order.js
+++ client/store/redux-order.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
+import getSingleOrder from './redux-single-order';
 
 // [] Get all orders for a certain User
-// For Admin only
+
 /**
  * ACTION TYPES
  */
 export const GET_ORDERS = 'GET_ORDERS';
-
 
 /**
  * ACTION CREATORS
@@ -18,14 +18,30 @@ export const getOrders = (orders) => ({ type: GET_ORDERS, orders });
 /**
  * THUNK CREATORS
  */
-export const fetchOrders = () => async (dispatch) => {
+// get only the orders that belong to the user
+
+export const fetchOrders = (userId) => async (dispatch) => {
   try {
-    const { data } = await axios.get('/api/orders');
+    const { data } = await axios.get(`/api/users/${userId}/orders/`);
     dispatch(getOrders(data));
   } catch (error) {
     console.log(error, 'Cool cliques throw bricks but seldom hit targets.');
   }
 };
+export const fetchUserOrder = (orderId) => async (dispatch) => {
+  //this request is sending a literal string and one piece of the string is a variable which is why you need this syntax
+  //I need to define my address because I need to specify what I want from the database
+  // I need to account for when :id changes.
+  try {
+    const res = await axios.get(`/api/orders/${orderId}`);
+    dispatch(getSingleOrder(res.data));
+  } catch (error) {
+    console.log(error, 'Cool cliques throw bricks but seldom hit targets.');
+  }
+};
+// --------------------------------------------------------------------
+
+//get order id from the url params
 /**
  * INITIAL STATE
  */
